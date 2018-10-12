bc9da74f3bf62bd1e0b9df26a3c6f6d67a098572
Tiernan O'Rourke <torourk@gmu.edu>
2018-10-12 14:30:15 -0400

    Changed toString to use weightString

--- src/Item.java
+++ src/Item.java
@@ -29,8 +29,7 @@ public class Item{
 	// To String Method
 	public String toString(){
 		String weightString = String.format("%.2f",itemWeight);
-		return itemName + "(" + itemWeight + "lb, $" + itemValue +")";
-
+		return itemName + "(" + weightString + " lb, $" + itemValue +")";
 	}
 
 }
