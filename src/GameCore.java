b00df98fbd508c7ac33a4f0e756580187e00c6a6
Shayan Amirhosseini <shawn.hossini@gmail.com>
2018-10-23 20:09:52 -0400

    {

--- src/GameCore.java
+++ src/GameCore.java
@@ -246,7 +246,8 @@ public class GameCore implements GameCoreInterface {
     * @return Message showing success.
     */
     @Override
-    public String whisper(String name1, String name2, String message) {
+    public String whisper(String name1, String name2, String message)
+    {
         Player playerSending = this.playerList.findPlayer(name1);
         Player playerReceiving = this.playerList.findPlayer(name2);
 	
