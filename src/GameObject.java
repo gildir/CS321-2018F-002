818dc7da919c8abac3c91cb6e1acf3653bec6bed
Ryan <rhoang@gmu.edu>
2018-10-23 22:59:49 -0400

    Ready For Commit

--- src/GameObject.java
+++ src/GameObject.java
@@ -11,15 +11,15 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class GameObject extends UnicastRemoteObject implements GameObjectInterface {
     private final GameCore core;
-
+    
     /**
      * Creates a new GameObject.  Namely, creates the map for the rooms in the game,
      *  and establishes a new, empty, player list.
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     public GameObject() throws RemoteException {
         super();
-
+        
         core = new GameCore();
     }
 
@@ -36,110 +36,110 @@ public class GameObject extends UnicastRemoteObject implements GameObjectInterfa
             return true;
         }
         return false;
-    }
-
-
+    }    
+  
+    
     /**
      * Allows a player to join the game.  If a player with the same name (case-insensitive)
-     *  is already in the game, then this returns false.  Otherwise, adds a new player of
+     *  is already in the game, then this returns false.  Otherwise, adds a new player of 
      *  that name to the game.  The next step is non-coordinated, waiting for the player
      *  to open a socket for message events not initiated by the player (ie. other player actions)
      * @param name
      * @return true is player is added, false if player name is already registered to someone else
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     @Override
     public boolean joinGame(String name) throws RemoteException {
         // Request join to the core and return the results back to the remotely calling method.
         return (core.joinGame(name) != null);
     }
-
+        
     /**
      * Returns a look at the area of the specified player.
      * @param playerName Player Name
      * @return String representation of the current area the player is in.
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     @Override
     public String look(String playerName) throws RemoteException {
         return core.look(playerName);
-    }
-
+    }        
+     
     /**
      * Turns the player left.
      * @param name Player Name
      * @return String message of the player turning left.
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     @Override
     public String left(String name) throws RemoteException {
         return core.left(name);
     }
-
+       
     /**
      * Turns the player right.
      * @param name Player Name
      * @return String message of the player turning right.
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     @Override
     public String right(String name) throws RemoteException {
         return core.right(name);
-    }
-
+    }    
+       
     /**
      * Says "message" to everyone in the current area.
      * @param name Name of the player to speak
      * @param message Message to speak
      * @return Message showing success.
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     @Override
     public String say(String name, String message) throws RemoteException {
         return core.say(name, message);
     }
-
+      
     /**
      * Attempts to walk forward < distance > times.  If unable to make it all the way,
      *  a message will be returned.  Will display LOOK on any partial success.
      * @param name Name of the player to move
      * @param distance Number of rooms to move forward through.
      * @return Message showing success.
-     * @throws RemoteException
+     * @throws RemoteException 
      */
     @Override
     public String move(String name, int distance) throws RemoteException {
         return core.move(name, distance);
     }
-
+      
     /**
      * Attempts to pick up an object < target >. Will return a message on any success or failure.
      * @param name Name of the player to move
      * @param target The case-insensitive name of the object to pickup.
      * @return Message showing success.
-     * @throws RemoteException
-     */
+     * @throws RemoteException 
+     */    
     @Override
     public String pickup(String name, String target) throws RemoteException {
         return core.pickup(name, target);
-    }
-
+    }    
+    
     /**
      * Returns a string representation of all objects you are carrying.
      * @param name Name of the player to move
      * @return Message showing success.
-     * @throws RemoteException
-     */
+     * @throws RemoteException 
+     */    
     @Override
     public String inventory(String name) throws RemoteException {
         return core.inventory(name);
-    }
-
+    }    
+    
      /**
      * Leaves the game.
      * @param name Name of the player to leave
-     * @throws RemoteException
-     */
+     * @throws RemoteException 
+     */    
     @Override
     public void leave(String name) throws RemoteException {
         Player player = core.leave(name);
