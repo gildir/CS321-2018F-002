3fb7dae64c908b5bb7145b6008a6ad6fe5678ad4
Bryan Ruano <bruanosa@masonlive@gmu.edu>
2018-11-02 14:54:06 -0400

    added random flavor to items in item class

--- src/Item.java
+++ src/Item.java
@@ -3,12 +3,24 @@ public class Item{
  private String itemName;
  private double itemWeight;
  private double itemValue;
+ private String itemColor;
+ private String itemSize;
+ private static String color[] = {"Red", "Blue", "Green", "Purple", "Orange", "Black", "White", "Brown", "Yellow", "Silver", "Gold","Pink"};
+ private static String size[] = {"Tiny", "Small", "Medium", "Large", "Huge"};
 
  // Constructor
  Item(String itemName, double itemWeight, double itemValue){
   this.itemName = itemName;
   this.itemWeight = itemWeight;
   this.itemValue = itemValue;
+  this.itemColor = getRandColor();
+  this.itemSize = getRandSize();
+ }
+ Item(String itemName, double itemWeight, double itemValue, String itemColor, String itemSize){
+    this.itemName = itemName;
+    this.itemWeight = itemWeight;
+    this.itemValue = itemValue;
+    this.itemColor = itemColor;
  }
 
  // Getter functions
@@ -21,15 +33,34 @@ public class Item{
  public double getItemValue(){
   return this.itemValue;
  }
+ public String getItemColor(){
+     return this.itemColor;
+ }
+ public String getItemSize(){
+     return this.itemSize;
+ }
  // Setter functions
  public void setItemValue(double newItemValue){
   this.itemValue = newItemValue;
  }
+ 
  // To String Method
  public String toString(){
   String weightString = String.format("%.2f", itemWeight);
   String valueString = String.format("%.2f", itemValue);
-  return itemName + "(" + weightString + " lb, $" + valueString +")";
+  return itemSize + itemColor + itemName + "(" + weightString + " lb, $" + valueString +")";
+ }
+ // Method spits out a random color
+ private String getRandColor()
+ {
+    Random rand = new Random();
+    return Item.color[rand.nextInt(color.length)];
+ }
+ //Method spits out a random size
+ private String getRandSize()
+ {
+     Random rand = new Random();
+     return Item.size[rand.nextInt(size.length)];
  }
 
 }
